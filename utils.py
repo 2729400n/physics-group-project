2d1b9120c2cbdd6d4acac1dfe28cd8d919bf26f6
2729400n <2729400n@student.gla.ac.uk>
2025-01-30 03:47:32 +0000

    Updated the inplace FTCS approach

--- utils.py
+++ utils.py
@@ -4,11 +4,17 @@ import scipy.optimize as optimist
 import matplotlib.pyplot as plt
 import sys,os
 
-print(os.name and sys.platform)
+
+# we will be using 64bit floating point representation
+# Stay clear of recursion if possible it a bad game to play unless you have 
+# a hop up or hop out scheme
+
+
+
 # Some easy to remember utils for readability 
 doNothing  = lambda x:x
 
-defualtResolutions = {''}
+defualtResolutions = {'1080i':'Nice Try!☺'}
 
 # We gonna solve this in a suitably fashion guys ☺
 def laplace_ode_solver(size:'tuple[int,int]|np.ndarray[int,int]',resoultion:'str|tuple[int,int]|np.ndarray[int,int]',fixedCondtions:'function'=doNothing,startingshape:'function'=doNothing):
@@ -24,4 +30,13 @@ def laplace_ode_solver(size:'tuple[int,int]|np.ndarray[int,int]',resoultion:'str
         A NumPy array representing the electric potential at all grid points.
     """
     # wow Gemini Docstrings kinda magic and good. Also rotationless solver
+    w_x_h = np.array(size,np.float64)
+    preception = np.array(resoultion,np.float64)
+    pixel_w_X_h = w_x_h/preception
+
+    # Three frames to allow rotation/Cyling and comparisons
+    # TODO: Check if it works with 2 Frames saved will make it less memory  expensive
+    Frames  = np.zeros((3,*pixel_w_X_h))
+
     
+
