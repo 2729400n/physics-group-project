04cc10f7490d71935c37e59c64b4f198ea75f176
2729400n <2729400n@student.gla.ac.uk>
2025-02-04 01:52:17 +0000

    doc appendix and some committed

--- Numerical Methods/GUI/DisplayPage.py
+++ Numerical Methods/GUI/DisplayPage.py
@@ -2,15 +2,20 @@ import tkinter as tk
 import tkinter.filedialog
 import turtle
 
+import numpy as np
+
+
+
 
 class ShapeDisplay(tk.Frame):
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
         self.setup()
+
     def setup(self):
         self.paint_canvas = tk.BooleanVar(self,False,name='paint_canvas')
 
-        self.heatMap = turtle.ScrolledCanvas(self,)
+        self.heatMap = tk.Canvas(self,)
         self.heatMap.bind('<ButtonPress-1>',self.begin_paint,add=True)
         self.heatMap.bind('<ButtonRelease-1>',self.end_paint,add=True)
         self.heatMap.pack()
@@ -19,17 +24,24 @@ class ShapeDisplay(tk.Frame):
     def begin_paint(self,event,*args, **kwargs):
         if not self.getvar('paint_canvas'):
             self.paint_canvas.set(True)
+            self.rectangles=[]
             self.tracer=self.heatMap.bind('<Motion>',self.canvas_mouse,add=True)
         self.pos0 = self.setvar(name='pos0',value=f'{event.x},{event.y}')
+        
     def canvas_mouse(self,motion:tk.Event,*args,**kwargs):
         if self.getvar('paint_canvas'):
             pos0 = [int(i.strip()) for i in self.getvar('pos0').split(',')]
-            self.heatMap.create_rectangle(-1.0,-1.0,motion.x/self.heatMap.canvwidth,motion.y/self.heatMap.canvheight,fill='green')
+            # base = np.array((-.5*self.heatMap.canvwidth,-.5*self.heatMap.canvheight))
+            self.heatMap.delete(*self.rectangles)
+            self.rectangles+=[self.heatMap.create_rectangle(*pos0,motion.x,motion.y)]
+            
             print(pos0[0],pos0[1],motion.x,motion.y)
             print(motion.x,motion.y)
         
     def end_paint(self,event,*args,**kwargs):
         if self.getvar('paint_canvas'):
+            if(self.rectangles.__len__()>1):
+                self.heatMap.delete(self.rectangles[:-1])
             self.paint_canvas.set(False)
             self.heatMap.unbind('<Motion>',self.tracer)
     def reload(self):
@@ -38,5 +50,19 @@ class ShapeDisplay(tk.Frame):
     def reshape(self):
         self.reload()
 
+class ControlPanel(tk.Frame):
+    def __init__(self):
+        super().__init__()
+    def init_controls(self):
+        self.shape=tk.Radiobutton(
+            self,command="Shape"
+        )
+class SetFieldPage(tk.Frame):
+    def __init__(self,*args,**kwargs):
+        super().__init__(args,kwargs)
+    def setup(self):
+        self.controlPanel= ControlPanel(self,)
+        self.fieldPanel=ShapeDisplay(self)
+    pass
 shaper = ShapeDisplay()
 tk.mainloop()
