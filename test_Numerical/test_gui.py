2ec7299ec53c7ecca23f0aa8c6e8ea546a226bfc
2729400n <2729400n@student.gla.ac.uk>
2025-02-06 22:21:24 +0000

    Add unit tests for custom colormap and solver functions; update imports and VSCode settings

--- test_Numerical/test_gui.py
+++ test_Numerical/test_gui.py
@@ -1,9 +1,14 @@
 import Numerical_Methods.GUI.gui_window_main as mainWin
 import sys,pytest
+import check_user_approval, unittest
 class Test_GUI():
     def test_scenes(self):
-        print(self,flush=True,file=sys.stderr)
-        sys.stderr.write('Hello')
-        raise Exception(self.__str__())
+        assert(check_user_approval.didItWorkAsIntended()==0)
+        return 0
     def test_show(self):
         mainWin.testProd()
+        assert(check_user_approval.didItWorkAsIntendedScale()>5,'Not satisfactory')
+        return 0
+@unittest.FunctionTestCase
+def test_canvas():
+    pass
