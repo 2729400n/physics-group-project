5a096722be6cc4f227acf4ef96caa23fae9fba40
2729400n <2729400n@student.gla.ac.uk>
2025-02-07 21:46:36 +0000

    added static and template directories to settings.py

--- phys_web_iface/settings.py
+++ phys_web_iface/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'phys_web_iface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['plot_viewer/static/templates'],
+        'DIRS': ['plot_viewer/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "plot_viewer/static/",
+    BASE_DIR /"static/",
+]
