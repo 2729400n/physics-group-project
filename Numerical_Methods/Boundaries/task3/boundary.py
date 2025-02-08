ac955bca8aa8cda8d78ead66641e7c7542a3a054
2729400n <2729400n@student.gla.ac.uk>
2025-02-08 21:37:21 +0000

    Added import and geomtry skeleton to task3

--- Numerical_Methods/Boundaries/task3/boundary.py
+++ Numerical_Methods/Boundaries/task3/boundary.py
@@ -3,5 +3,8 @@ import numpy as np
 
 def highSpeedFactory(width, height, rects:'Rectangle_x4'=None,spacings=None,padding:'np.ndarray'=None):
     def highSpeedGeometry(Grid:np.ndarray,overlay=None,retoverlay=False):
-        for i in rect
+        
+        out = None
+        return out
+    return highSpeedGeometry
     
