15b961e18a02df4625c66a8e98f6c0e12bf90e81
2729400n <2729400n@student.gla.ac.uk>
2025-02-08 22:51:57 +0000

    Added a much more workable solution for task1

--- Numerical_Methods/geometry.py
+++ Numerical_Methods/geometry.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def circle(cx,cy,r,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
     # First solve for a qudrant the apply rotations
-    operations = [[lambda x,y,z:np.abs(x-y)<z]*2,[lambda x,y,z:x<=y,lambda x,y,z:x>=y]]
+    operations = [[lambda x,y,z:np.abs(x-y)<=z]*2,[lambda x,y,z:x<=y,lambda x,y,z:x>=y]]
     
     # === Fast Slow pointer ===
     # r1 = np.array([0,r])
@@ -22,10 +22,10 @@ def circle(cx,cy,r,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.
         # print(x,y)
 
     grid_x, grid_y = np.mgrid[:y, :x]
-    circle_mask = operations[bool(fill)][bool(clear)]((grid_x-cx)**2 + (grid_y-cy)**2 , r**2,7)
+    circle_mask = operations[bool(fill)][bool(clear)]((grid_x-cx)**2 + (grid_y-cy)**2 , r**2,100)
     # Apply anti-aliasing using Gaussian blur
     # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
-    vals = (1,0) if clear else (0,1)
+    vals = (1,0) 
     # Threshold to create a binary image (0 or 1)
     pixelated_circle = np.where(circle_mask == True, *vals)
 
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     import matplotlib.artist,matplotlib.patches,matplotlib.path,matplotlib.pyplot as plt
     grid = np.zeros((200,200))
     grid[:,:]=1
-    circ=circle(50,50,50,1,1,fill=False,clear=True,Grid=grid)
+    circ=circle(100,100,100,1,1,fill=True,clear=True,Grid=grid)
         
     plt.imshow(circ, cmap='gray')
     plt.colorbar()
