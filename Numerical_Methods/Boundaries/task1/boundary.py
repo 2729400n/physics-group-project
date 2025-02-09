b7feb6bb30054356986561b136221b0f4ad0e225
2729400n <2729400n@student.gla.ac.uk>
2025-02-09 15:57:10 +0000

    added runners

--- Numerical_Methods/Boundaries/task1/boundary.py
+++ Numerical_Methods/Boundaries/task1/boundary.py
@@ -3,9 +3,13 @@ import numpy as np
 
 
 def geometryFactory(val=1.0,r1=35,r2=50,cx=50,cy=100,relative=False):
+    # A simple cacher 
     Gridder = None
     def endToEndLine(Grid:np.ndarray,overlay=None,retoverlay=False):
-        width,height = Grid.shape
-        circ1 = annulus(100,100,50,r1,r2,fill=True,clear=False,Grid=grid)
-        return Grid
+        nonlocal Gridder
+        if Gridder is None:
+            width,height = Grid.shape
+            circ1 = annulus(100,100,50,r1,r2,fill=True,clear=False,Grid=Grid)
+            Gridder = circ1
+        return Grid*Gridder
     return endToEndLine
