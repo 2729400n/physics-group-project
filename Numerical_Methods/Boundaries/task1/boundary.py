f69c36f0564c0a6223800aaa4c4140ed42bf9b16
2729400n <2729400n@student.gla.ac.uk>
2025-02-09 16:00:15 +0000

    Added some explanation

--- Numerical_Methods/Boundaries/task1/boundary.py
+++ Numerical_Methods/Boundaries/task1/boundary.py
@@ -2,14 +2,15 @@ from ...utilis.geometry import circle,annulus
 import numpy as np
 
 
+# Will employ the use of a closure to ensure uniqueness for multi-threading
 def geometryFactory(val=1.0,r1=35,r2=50,cx=50,cy=100,relative=False):
     # A simple cacher 
     Gridder = None
-    def endToEndLine(Grid:np.ndarray,overlay=None,retoverlay=False):
+    def AnnulusField(Grid:np.ndarray,overlay=None,retoverlay=False):
         nonlocal Gridder
         if Gridder is None:
             width,height = Grid.shape
             circ1 = annulus(100,100,50,r1,r2,fill=True,clear=False,Grid=Grid)
             Gridder = circ1
         return Grid*Gridder
-    return endToEndLine
+    return AnnulusField
