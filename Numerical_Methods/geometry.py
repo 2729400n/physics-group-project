b6af3531affdbd4a28e36f91f0c9a36d4fa7ccb1
2729400n <2729400n@student.gla.ac.uk>
2025-02-09 01:42:57 +0000

    Added Geometric representation for an anulus

--- Numerical_Methods/geometry.py
+++ Numerical_Methods/geometry.py
@@ -35,6 +35,34 @@ def circle(cx,cy,r,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.
     
     return pixelated_circle
 
+def annulus(cx, cy, r1, r2, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
+    # First solve for a qudrant the apply rotations
+    operations = [[lambda w,x,y,z:(np.abs(w-y)<=z)and(np.abs(w-y)<=z)]*2,[lambda w,x,y,z:x<=w<=y,lambda w,x,y,z:(not (x<=w<=y))]]
+
+    
+    # if all we want is a  circle
+    x = 2*int(r//dx)+5
+    y=2*int(r//dy)+5
+    if type(Grid) == tuple:
+        Grid = np.full(Grid,1)
+    # if we inplace into a grid
+    if Grid is not None:
+        y,x = Grid.shape
+        # print(x,y)
+
+    grid_x, grid_y = np.mgrid[:y, :x]
+    circle_mask = operations[bool(fill)][bool(clear)]((grid_x-cx)**2 + (grid_y-cy)**2 , r1**2,r2,100)
+    # Apply anti-aliasing using Gaussian blur
+    # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
+    vals = (1,0) 
+    # Threshold to create a binary image (0 or 1)
+    pixelated_circle = np.where(circle_mask == True, *vals)
+
+    # mul mask
+    if Grid is not  None:
+        return Grid*pixelated_circle
+    
+    return pixelated_circle
 
 # deal with essy overlays
 def identityOverlay(Grid:np.ndarray):
