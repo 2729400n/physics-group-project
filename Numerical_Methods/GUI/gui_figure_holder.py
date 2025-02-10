b61ea5d5fcfdae37374488ffdbcea0573784b579
2729400n <2729400n@student.gla.ac.uk>
2025-02-10 19:31:17 +0000

    Added commented Working solution

--- Numerical_Methods/GUI/gui_figure_holder.py
+++ Numerical_Methods/GUI/gui_figure_holder.py
@@ -8,10 +8,15 @@ class FigureBase(tk.Widget):
     def  __init__(self):
         tk.PhotoImage('::tk::images::img',file=r'c:\Users\Kevnn\GroupProject\physics-group-project\BoxInBox.png')
         
-root=tk.Tk()
-img=tk.PhotoImage('::tk::images::img',file='c:\\Users\\Kevnn\\GroupProject\\physics-group-project\\BoxInBox.png',master=root,width=640,height=480)
-lab=tk.Label(image=img,master=root,)
-lab.pack()
-root.update()
-root.mainloop()
-print()
+        
+        
+# root=tk.Tk()
+# img=tk.PhotoImage('::tk::images::img',file='c:\\Users\\Kevnn\\GroupProject\\physics-group-project\\BoxInBox.png',master=root)
+# xdownsize = int(img.width()//640)
+# ydownsize = int(img.height()//480)
+# img = img.subsample(xdownsize,ydownsize)
+# lab=tk.Label(image=img,master=root,width=640,height=480)
+# lab.pack()
+# root.update()
+# root.mainloop()
+# print()
