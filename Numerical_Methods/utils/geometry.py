de88501ce75bac4b0fbcf431452746be48215b53
2729400n <2729400n@student.gla.ac.uk>
2025-02-11 15:33:59 +0000

    Added a new rectangle geometry

--- Numerical_Methods/utils/geometry.py
+++ Numerical_Methods/utils/geometry.py
@@ -28,7 +28,7 @@ def circle(cx,cy,r,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.
     # TODO Variate Tolerance : the cirlces tolerance for a non filled circle should be a function of the radius 
     # Currently not implemented correctly it causes a band instead of a line this band width can variet based on the tolerance
      
-    circle_mask = operations[bool(fill)][bool(clear)]((grid_x-cx)**2 + (grid_y-cy)**2 , r**2,100)
+    circle_mask = operations[bool(fill)][bool(clear)]((grid_x-cx)**2 + (grid_y-cy)**2 , r**2, r)
     # Apply anti-aliasing using Gaussian blur
     # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
     vals = (1,0) 
@@ -45,7 +45,7 @@ def circle(cx,cy,r,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.
 def annulus(cx, cy, r1, r2, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
     # First solve for a qudrant the apply rotations
     # TODO Add Finer Control: Could someone add finer controls to allow for control over the inner and outer values of the annulus 
-    operations = [[lambda w,x,y,z:np.logical_or(np.abs(w-x)<=z,np.abs(w-y)<=z)]*2,[lambda w,x,y,z:np.logical_and(x<=w,w<=y),lambda w,x,y,z:(np.logical_or(x>w,w>y))]]
+    operations = [[lambda w,x,y,z,zz:np.logical_or(np.abs(w-x)<=z,np.abs(w-y)<=zz)]*2,[lambda w,x,y,z,zz:np.logical_and(x<=w,w<=y),lambda w,x,y,z,zz:(np.logical_or(x>w,w>y))]]
 
     grid_class=type(Grid)
     
@@ -57,7 +57,7 @@ def annulus(cx, cy, r1, r2, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'
         # print(x,y)
 
     grid_x, grid_y = np.mgrid[:y, :x]
-    annulus_mask = operations[bool(fill)][bool(clear)]( ((grid_x-cx)**2 + (grid_y-cy)**2 ), r1**2, r2**2,100 )
+    annulus_mask = operations[bool(fill)][bool(clear)]( ((grid_x-cx)**2 + (grid_y-cy)**2 ), r1**2, r2**2,r1,r2 )
     # Apply anti-aliasing using Gaussian blur
     # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
     vals = (1,0) 
@@ -78,8 +78,28 @@ def rectangle(x,y,w,h,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[
     if Grid is not None:
         y,x = Grid.shape
         # print(x,y)
-    grid_x, grid_y = np.mgrid[:y, :x]
-    annulus_mask = operations[bool(fill)][bool(clear)]( ((grid_x-cx)**2 + (grid_y-cy)**2 ), r1**2, r2**2,100 )
+    grid_x, grid_y = np.full((max(0,x0)))
+    # Apply anti-aliasing using Gaussian blur
+    # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
+    vals = (1,0) 
+    # Threshold to create a binary image (0 or 1)
+    pixelated_annulus = np.where(annulus_mask == True, *vals)
+
+    # mul mask
+    if Grid is not  None:
+        if(grid_class!=tuple):
+            return Grid*pixelated_annulus
+    
+    return pixelated_annulus 
+
+def rectangle(x,y,w,h,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
+    operations = []
+    if type(Grid) == tuple:
+        Grid = np.full(Grid,1)
+    if Grid is not None:
+        y,x = Grid.shape
+        # print(x,y)
+    grid_x, grid_y = np.full((max(0,x0)))
     # Apply anti-aliasing using Gaussian blur
     # blurred_circle = gaussian_filter(circle_mask.astype(float), sigma=antialias_sigma)
     vals = (1,0) 
@@ -103,9 +123,9 @@ def rectangle(x0,y0,x1,y1,theta=0,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:
 
 if __name__ == '__main__':
     import matplotlib.artist,matplotlib.patches,matplotlib.path,matplotlib.pyplot as plt
-    grid = np.zeros((200,200))
+    grid = np.zeros((500,500))
     grid[:,:]=1
-    circ=annulus(100,100,50,100,2,fill=False,clear=False,Grid=grid)
+    circ=annulus(100,100,25,100,2,fill=False,clear=False,Grid=grid)
         
     plt.imshow(circ, cmap='gray')
     plt.colorbar()
