e6279c9b0fa9ce9ceb2d1610c471cb0e1b8aea07
2729400n <2729400n@student.gla.ac.uk>
2025-02-11 19:29:51 +0000

    Reshuffled Scenes so they are now containerised

--- Numerical_Methods/GUI/tabbed_view/__init__.py
+++ Numerical_Methods/GUI/tabbed_view/__init__.py
@@ -1,12 +1,18 @@
+import importlib.machinery
+import importlib.util
 import tkinter as tk
 import tkinter.ttk as ttk
 from typing import Iterable
 
-def buildTab():
+import importlib
+
+mod :importlib.machinery.ModuleSpec = importlib.util.spec_from_file_location()
+mod.loader.load_module()
+def buildTab(dir):
     tk.Tk()
     nb=ttk.Notebook(width=640,height=480)
     mf=tk.Frame(nb,width=640,height=480)
-    nb.add(mf,state='normal',sticky='nesw',text='mainFrame')
+    nb.add(mf,state='normal',sticky='nesw',text='mainFrame',)
     nb.pack()
     tk.mainloop()
 
