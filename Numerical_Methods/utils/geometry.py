4b37ebf574a77d6e1e41bdd102e20b67e8d79fe6
2729400n <2729400n@student.gla.ac.uk>
2025-02-11 16:08:44 +0000

    Added rectangle geometry

--- Numerical_Methods/utils/geometry.py
+++ Numerical_Methods/utils/geometry.py
@@ -69,21 +69,39 @@ def annulus(cx, cy, r1, r2, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'
     return pixelated_annulus
 
 
-def rectangle(x0, y0, x1, y1, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
-    operations = []
+def rectangle(x0, y0, x1, y1, dx=1, dy=1, val=1.0, fill=False, clear=False, Grid:'np.ndarray[np.ndarray[np.float64]]'=None,blocking=False):
+    
+    grid_class = type(Grid)
+    
     if type(Grid) == tuple:
         Grid = np.full(Grid,1)
+    
     if Grid is not None:
         y,x = Grid.shape
-        # print(x,y)
-    
+    if not blocking:
+        x1 = None if x1 > x else x1
+        x0 = None if x0 < 0 else x0
 
-    # mul mask
+        y1 = None if y1 > y else y1
+        y0 = None if y0 < 0 else y0
+    else:
+        x1 = min(max(x-1,0),x1)
+        x0 = max(0,x0)
+
+        y1 = min(max(y-1,0),y1)
+        y0 = max(0,y0)
+    
+    mul_mask = np.full_like(Grid,1)
+    
+    if fill:
+        mul_mask[y0:y1,x0:x1] = 0 if clear else 1
+    else:
+        mul_mask[tuple( [i for i in (y0,y1) if i is not None]  ),x0:x1] = mul_mask[y0:y1, tuple( [i for i in (x0,x1) if i is not None]  )] = 0 if clear else 1
     if Grid is not  None:
         if(grid_class!=tuple):
-            return Grid*pixelated_annulus
+            return Grid*mul_mask
     
-    return pixelated_annulus
+    return mul_mask
 
 # A wrapper function for Rectangle 
 def rectangle_w_h(x,y,w,h,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.ndarray[np.float64]]'=None):
@@ -92,7 +110,7 @@ def rectangle_w_h(x,y,w,h,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndar
     x1=x+w
     y0 = y
     y1 =y+h
-    return pixelated_annulus 
+    return rectangle(x0=x0, x1=x1, y0=y0, y1=y1, dx=dx, dy=dy,val=val,fill=fill,clear=clear,Grid=Grid) 
 
  
 
@@ -101,15 +119,22 @@ def identityOverlay(Grid:np.ndarray):
     return np.full_like(Grid,1)
 
 
-def rectangle(x0,y0,x1,y1,theta=0,dx=1,dy=1,val=1.0,fill=False,clear=False,Grid:'np.ndarray[np.ndarray[np.float64]]'=None)->np.ndarray:
-    grid_x, grid_y = np.mgrid[y0:y1, x0:x1]
+# if __name__ == '__main__':
+#     import matplotlib.artist,matplotlib.patches,matplotlib.path,matplotlib.pyplot as plt
+#     grid = np.zeros((500,500))
+#     grid[:,:]=1
+#     circ=annulus(100,100,25,100,2,fill=False,clear=False,Grid=grid)
+        
+#     plt.imshow(circ, cmap='gray')
+#     plt.colorbar()
+#     plt.title('Pixelated Circle with Anti-aliasing')
+#     plt.show()
 
 if __name__ == '__main__':
     import matplotlib.artist,matplotlib.patches,matplotlib.path,matplotlib.pyplot as plt
     grid = np.zeros((500,500))
     grid[:,:]=1
-    circ=annulus(100,100,25,100,2,fill=False,clear=False,Grid=grid)
-        
+    circ=rectangle_w_h(100,100,6000,200,fill=False,clear=True,Grid=grid)
     plt.imshow(circ, cmap='gray')
     plt.colorbar()
     plt.title('Pixelated Circle with Anti-aliasing')
