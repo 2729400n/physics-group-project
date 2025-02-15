eeb2b4a47fe2b8343376efe22faba699b1efc6eb
2729400n <2729400n@student.gla.ac.uk>
2025-02-15 18:32:22 +0000

    Fixed function signature assigner. It now add the positional arguments to the signature

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import inspect
 import numpy as np
+import scipy.optimize as optimist
+import numpy.linalg as linalg
+
+
 
 
 def functionMaker(n: int, m: int, dx: int = 1, dy: int = 1):
@@ -15,46 +19,91 @@ def functionMaker(n: int, m: int, dx: int = 1, dy: int = 1):
             result += args[-i] * (y ** (i))
         return np.float64(result)
 
-    def polyProduct(x, y, xcoeffs=None, ycoeffs=None):
-        return XLin(x, *xcoeffs) * YLin(y, ycoeffs)
+    
+    
+    xParam = [inspect.Parameter(
+                f"x",
+                inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
+                default=0,
+                annotation=np.float64,
+            )]
 
+    xCoeffParams = [
+            inspect.Parameter(
+                f"x_{i}",
+                inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
+                default=0,
+                annotation=np.float64,
+            )
+            for i in range(0, n, 1)
+        ]
+    
+    xParams  = xParam+xCoeffParams
+    
     XLin.__signature__ = inspect.Signature(
-        [
+        xParams,
+        return_annotation=np.float64,
+        __validate_parameters__=True,
+    )
+    
+    yParam = [
             inspect.Parameter(
-                f"a{i}",
+                f"y",
+                inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
+                default=0,
+                annotation=np.float64,
+            )]
+    
+    yCoeffParams = [
+            inspect.Parameter(
+                f"y_{i}",
                 inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
                 default=0,
                 annotation=np.float64,
             )
-            for i in range(0, n, 1)
-        ],
-        return_annotation=np.float64,
-        __validate_parameters__=True,
-    )
+            for i in range(0, m, 1)
+        ]
+    
+    yParams = yParam+yCoeffParams
+    
     YLin.__signature__ = inspect.Signature(
-        [
-            inspect.Parameter(
-                f"a{i}",
-                inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
-                default=0,
-                annotation=np.float64,
-            )
-            for i in range(0, n, 1)
-        ],
+        yParams,
         return_annotation=np.float64,
         __validate_parameters__=True,
     )
-    polyProduct.__signature__ = inspect.Signature(
-        [
-            inspect.Parameter(
-                f"a{i}",
+    
+    
+    def polyProduct(x, y, xcoeffs:'list[float]'=None, ycoeffs:'list[float]'=None,*coeffs):
+        coeff_len = len(coeffs)
+        return XLin(x, *xcoeffs) * YLin(y, ycoeffs) + sum([i for i in range(coeff_len)])
+    
+    mixedParam = [
+        inspect.Parameter(
+                f"x",
                 inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
                 default=0,
                 annotation=np.float64,
             )
-            for i in range(0, n, 1)
-        ],
+    ]
+    
+    mixedParams = [
+            inspect.Parameter(
+                f"c_{i}",
+                inspect._ParameterKind.POSITIONAL_OR_KEYWORD,
+                default=0,
+                annotation=np.float64,
+            )
+            for i in range(0, n*m, 1)
+        ]
+    
+    polyProduct.__signature__ = inspect.Signature(
+        mixedParams,
         return_annotation=np.float64,
         __validate_parameters__=True,
     )
     return XLin, YLin, polyProduct
+
+
+def InterpolateGrid(Grid:'np.ndarray'):
+    (n,m) = Grid.shape
+    
