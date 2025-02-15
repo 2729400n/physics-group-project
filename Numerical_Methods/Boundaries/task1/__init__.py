2fea5d2c4eb256f884d9bbb654488f9238cd6c7d
2729400n <2729400n@student.gla.ac.uk>
2025-02-15 05:32:42 +0000

    Updated teh first task skeleton to better match a local implematation and API

--- Numerical_Methods/Boundaries/task1/__init__.py
+++ Numerical_Methods/Boundaries/task1/__init__.py
@@ -1,8 +1,16 @@
 from .boundary import geometryFactory as Boundary
+import typing
 
-
-class Task1():
+class Task1(typing.Callable,typing.Protocol):
     def __init__(self):
         pass
-    def setup(self):
+    def _setup(self):
         pass
+    def _run(self):
+        pass
+    def _cleanup(self):
+        pass
+    def __call__(self,*args,**kwargs):
+        pass
+
+    
