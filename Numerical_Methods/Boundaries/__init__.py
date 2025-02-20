198051e723fcfa45cace64c711db1ca46c8c2a23
2729400n <2729400n@student.gla.ac.uk>
2025-02-20 22:42:03 +0000

    Added Convolutions

--- Numerical_Methods/Boundaries/__init__.py
+++ Numerical_Methods/Boundaries/__init__.py
@@ -2,3 +2,4 @@ from .task import Task, tasks
 from .task1 import Task1, Boundary as boundary1
 from .task2 import Task2, Boundary as boundary2
 from .task3 import Task3, Boundary as boundary3
+from .task5_ext import Task5, Boundary as boundary5
---
+++ Numerical_Methods/Solvers/ftcs_algebra.py
@@ -0,0 +1,115 @@
+# License: None
+# Author: P3 Comp. 2025 Lab Group B ( Electrostatics )
+# Description: A module of utilities for the Numerical Solver
+
+# Load modules and imports neceasasary 
+
+import numpy as np
+from ..utils import createConvMatrix,convolveMatrixes
+
+# we will be using 64bit floating point representation
+# Stay clear of recursion if possible it a bad game to play unless you have 
+# a hop up or hop out scheme
+
+
+
+# Some easy to remember utils for readability 
+def doNothing(x:'np.ndarray | tuple[int,int]'=None,*args,**kwargs):
+    if isinstance(x,tuple):
+        return np.zeros(x,np.float64)
+    return x
+
+
+
+
+# We gonna solve this in a suitably fashion guys ☺
+def laplace_ode_solver_8pointd(size:'tuple[int,int]|np.ndarray[int,int]', fixedCondtions:'function'=doNothing,startingshape:'function'=doNothing,resoultion:'str|tuple[int,int]|np.ndarray[int,int]'=(1,1), convMatrix:np.ndarray = None):
+    
+    # TODO: Fix docstrings adding more detail to params
+    """Solves the Laplace equation using a finite difference scheme.
+
+    Args:
+        size: A tuple (x_size, y_size) specifying the grid dimensions.
+        resolution: A tuple (dx, dy) specifying the grid spacing or a string 'auto' for automatic resolution.
+        fixedCondtions: A function that enforces boundary conditions on the potential grid.
+        startingshape: A function that defines the initial shape of the potential grid.
+
+    Returns:
+        A NumPy array representing the electric potential at all grid points.
+    """
+    
+    w_x_h = np.array(size,int)
+    preception = np.array(resoultion,int)
+    if convMatrix is None:
+        convMatrix = createConvMatrix(1/8)
+        
+    # pixel_w_X_h = w_x_h/preception
+    
+    # two frames to allow rotation/Cyling and comparisons
+    
+    Xs= np.arange(0,w_x_h[0]+resoultion[0],resoultion[0])
+    Ys = np.arange(0,w_x_h[1]+resoultion[1],resoultion[1])
+    # Frames  = np.zeros((2,int(pixel_w_X_h[1]),int(pixel_w_X_h[0])))
+    Frames  = np.zeros((2,Ys.shape[0],Xs.shape[0]))
+    Frames[0] = startingshape(Frames[0])
+    i = 0
+    # TODO: possibly remove while loop, its too messy.
+    while True:
+        
+        
+
+        Frames[(i+1)%2, 1:-1, 1:-1] = convolveMatrixes(Frames[i%2], convMatrix,wrap=True)[1:-1, 1:-1]
+        Frames[(i+1)%2] = fixedCondtions(Frames[(i+1)%2])
+        indexes=Frames[i%2]!=0
+        diff = (np.abs((Frames[0][indexes]-Frames[1][indexes]))/Frames[i%2][indexes])
+        i= (i+1)
+        
+        # TODO: Make the change relative easier to tell the precentage change
+        if np.max(diff) < 1e-6 and i>1:
+            break
+    retvals = (Ys,Xs,Frames[i%2])
+    
+    return retvals
+
+
+# We gonna solve this in a suitably fashion guys ☺
+def laplace_ode_solver_8pointd_continue(Grid: 'np.ndarray[np.ndarray[np.float64]]', fixedCondtions:'function'=doNothing,startingshape:'function'=doNothing,resoultion:'str|tuple[int,int]|np.ndarray[int,int]'=(1,1), convMatrix:np.ndarray = None):
+    
+    # TODO: Fix docstrings adding more detail to params
+    """Solves the Laplace equation using a finite difference scheme.
+
+    Args:
+        size: A tuple (x_size, y_size) specifying the grid dimensions.
+        resolution: A tuple (dx, dy) specifying the grid spacing or a string 'auto' for automatic resolution.
+        fixedCondtions: A function that enforces boundary conditions on the potential grid.
+        startingshape: A function that defines the initial shape of the potential grid.
+
+    Returns:
+        A NumPy array representing the electric potential at all grid points.
+    """
+    
+    Frames  = np.zeros((2,*Grid.shape),dtype=np.float64)
+    Frames[0] = startingshape(Grid)
+    Xs = np.arange(0,Grid.shape[1]+1,1)
+    Ys = np.arange(0,Grid.shape[0]+1,1)
+    print(Xs.shape[0],Ys.shape[0])
+    i = 0
+    # TODO: possibly remove while loop, its too messy.
+    if convMatrix is None:
+        convMatrix = createConvMatrix(1/8)
+    for i in range(2):
+        
+        
+
+        Frames[(i+1)%2] = convolveMatrixes(Frames[i%2], convMatrix,wrap=False)
+        Frames[(i+1)%2] = fixedCondtions(Frames[(i+1)%2])
+        indexes=Frames[i%2]!=0
+        diff = (np.abs((Frames[0][indexes]-Frames[1][indexes]))/Frames[i%2][indexes])
+        i= (i+1)
+        
+        # TODO: Make the change relative easier to tell the precentage change
+        if np.max(diff) < 1e-6 and i>1:
+            break
+    retvals = (Ys,Xs,Frames[i%2])
+    
+    return retvals
