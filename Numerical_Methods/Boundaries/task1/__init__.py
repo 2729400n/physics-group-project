2e3f36d3108e0341e3cf16b90ed6649d1bc9eefd
2729400n <2729400n@student.gla.ac.uk>
2025-02-21 12:12:13 +0000

    Continuing effort to format files for readability

--- Numerical_Methods/Boundaries/task1/__init__.py
+++ Numerical_Methods/Boundaries/task1/__init__.py
@@ -1,90 +1,86 @@
-from matplotlib import rc
 from matplotlib.colorbar import Colorbar
 from matplotlib.quiver import Quiver
 import numpy as np
 from .boundary import geometryFactory as Boundary
-from ...Solvers import laplace_ode_solver, findUandV,laplace_ode_solver_step,laplace_ode_solver_continue
-import typing
-import tkinter as tk
+from ...Solvers import findUandV, laplace_ode_solver_continue
 from matplotlib.axes import Axes
 from matplotlib.image import AxesImage
 from ..task import Task
 
 
-
 class Task1(Task):
-    name="Anulus"
+    name = "Anulus"
+
     def __init__(self, axes: 'Axes' = None, *args, **kwargs):
         super().__init__()
-        self.Image : AxesImage = None
-        self.grid=None
-        
+        self.Image: AxesImage = None
+        self.grid = None
 
-    def setup(self, x1: float, y1: float,r1:float,r2:float,cx:float,cy:float,v:float=1.0,x0:float=0.0,y0:float=0.0,dy:float=1.0,dx:float=1.0):
-        x0,x1 = (x0,x1) if x0<=x1 else (x1,x0)
-        y0,y1 = (y0,y1) if y0<=y1 else (y1,y0)
-        print(x0,x1,dx)
-        Xs=np.arange(x0,x1+dx,dx)
-        Ys=np.arange(y0,y1+dy,dy)
-        
-        grid = np.zeros(shape=(Ys.shape[0],Xs.shape[0]),dtype=np.float64)
-        self.boundaryCondition=Boundary(v,r1,r2,cx,cy)
-        self.cbar:Colorbar = None
-        self.quivers:Quiver=None
-        self.grid = grid=self.boundaryCondition(Grid=grid,retoverlay=False)
-        self.Xs,self.Ys = np.mgrid[:grid.shape[1], :grid.shape[0]]
+    def setup(self, x1: float, y1: float, r1: float, r2: float, cx: float,
+              cy: float, v: float = 1.0, x0: float = 0.0, y0: float = 0.0,
+              dy: float = 1.0, dx: float = 1.0):
+
+        x0, x1 = (x0, x1) if x0 <= x1 else (x1, x0)
+        y0, y1 = (y0, y1) if y0 <= y1 else (y1, y0)
+        print(x0, x1, dx)
+        Xs = np.arange(x0, x1+dx, dx)
+        Ys = np.arange(y0, y1+dy, dy)
+
+        grid = np.zeros(shape=(Ys.shape[0], Xs.shape[0]), dtype=np.float64)
+        self.boundaryCondition = Boundary(v, r1, r2, cx, cy)
+        self.cbar: Colorbar = None
+        self.quivers: Quiver = None
+        self.grid = grid = self.boundaryCondition(Grid=grid, retoverlay=False)
+        self.Xs, self.Ys = np.mgrid[:grid.shape[1], :grid.shape[0]]
         self.resXs = Xs
         self.resYs = Ys
         self.resdx = dx
         self.resdy = dy
         axes = self.axes
         print(grid.shape)
-        img=self.Image = axes.imshow(grid)
-        
+        # img = self.Image = axes.imshow(grid)
+
         axes.set_title('Electrostatic Potential')
         self.figure.canvas.figure = None
         self.figure.canvas.figure = self.figure
         self.figure.canvas.draw_idle()
         print(self.grid)
-    
-    
+
     def redraw(self):
-        if(self.grid) is not None:
+        if (self.grid) is not None:
             return
         self.axes.clear()
-        img=self.axes.imshow(self.grid)
+        # img = self.axes.imshow(self.grid)
         return
-        
+
     def _show_Efield(self):
-        u_v= findUandV(grid=self.grid)[::5,::5]*25
+        u_v = findUandV(grid=self.grid)[::5, ::5]*25
         axes = self.axes
-        Xs = self.Xs[::5,::5]
-        Ys = self.Ys[::5,::5]
+        Xs = self.Xs[::5, ::5]
+        Ys = self.Ys[::5, ::5]
         if self.quivers is not None:
             self.quivers.set_visible(False)
             self.quivers.remove()
             self.quivers = None
-        quiv=axes.quiver(Xs, Ys, u_v[:,:,0], u_v[:,:,1], color='b', scale=0.1, scale_units='xy') # Adjust scale and color
-        self.quivers=quiv
-        
-        
-        
+        quiv = axes.quiver(Xs, Ys, u_v[:, :, 0], u_v[:, :, 1], color='b',
+                           scale=0.1, scale_units='xy')
+        self.quivers = quiv
+
     def run(self):
-        Xs,Ys,self.grid=laplace_ode_solver_continue(self.grid,self.boundaryCondition)
+        Xs, Ys, self.grid = laplace_ode_solver_continue(
+            self.grid, self.boundaryCondition)
         if self.cbar is not None:
             self.cbar.remove()
             self.cbar = None
             self.figure.clear()
-            axes=self.figure.add_subplot(111)
+            axes = self.figure.add_subplot(111)
             self.axes = axes
         self.axes.imshow(self.grid)
-        
+
         self.figure.draw_without_rendering()
         self.figure.colorbar(self.axes.images[0])
-        
-        self.figure.canvas.draw()
-        
 
+        self.figure.canvas.draw()
 
     def _cleanup(self):
         pass
--- non_final_main.py
+++ non_final_main.py
@@ -1,33 +1,29 @@
 import Numerical_Methods.GUI as gui
 import sys
 import test_Numerical.check_user_approval as check_user_approval
-import unittest
-import pytest
-import sys
 import threading
-import os
-import time
 import os.path as pth
 from importlib import reload
-import signal
+
 
 class Test_GUI():
+
     def test_scenes(self):
-        assert(check_user_approval.didItWorkAsIntended()==0)
+        assert (check_user_approval.didItWorkAsIntended() == 0)
         return None
+
     def test_show(self):
         gui.start()
-        # assert(check_user_approval.didItWorkAsIntendedScale()>5,'Not satisfactory')
         return None
 
 
 main_thread_running = False
 
+
 def updateModules():
     global main_thread_running
-    iptime ={}
+    iptime = {}
     notSpeced = []
-    updatDictionary ={}
     copydict = {}
     while main_thread_running:
         copydict = sys.modules.copy()
@@ -46,44 +42,44 @@ def updateModules():
                 continue
             if not pth.exists(modspec.origin):
                 continue
-            modtime =pth.getmtime(modspec.origin)
+            modtime = pth.getmtime(modspec.origin)
             if modspec.cached:
-                if  pth.exists(modspec.cached):    
+                if pth.exists(modspec.cached):
                     mtimecache = pth.getmtime(modspec.cached)
-                    if mtimecache<modtime:
+                    if mtimecache < modtime:
                         print(f'Reloaded {i}')
                         reload(mod)
                         continue
                     else:
                         continue
-            
+
             imptime = iptime.get(i)
             if imptime is None:
                 iptime[i] = modtime
                 continue
-            if imptime<modtime:
+            if imptime < modtime:
                 print(f'reloaded {i}')
                 reload(mod)
-                
+
                 iptime[i] = modtime
     return None
-                    
+
 
 if __name__ == "__main__":
-    updateThread=threading.Thread(None,updateModules,'updateModules',daemon=False)
+    updateThread = threading.Thread(
+        None, updateModules, 'updateModules', daemon=False)
     tester = Test_GUI()
-    
+
     main_thread_running = True
     updateThread.start()
     try:
         tester.test_show()
-        
+
         main_thread_running = False
         updateThread.join(9)
-    except:
-        main_thread_running=False
+    except Exception as e:
+        main_thread_running = False
         updateThread.join(9)
+        raise e
     finally:
         exit(1)
-    
-    
