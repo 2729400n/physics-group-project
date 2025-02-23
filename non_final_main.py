ae7207d29cf5385daee4f52fae091b91810dcb2e
2729400n <2729400n@student.gla.ac.uk>
2025-02-23 16:11:36 +0000

    ReAdded the Realtime module loader library.

--- non_final_main.py
+++ non_final_main.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
             rtLoader.stop_realtime_module()
             raise e
         finally:
+            rtLoader.stop_realtime_module()
             exit(1)
     else:
         try:
