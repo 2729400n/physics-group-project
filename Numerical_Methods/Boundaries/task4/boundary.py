c3a35c506a282c2bc9ed3c51dcbf8707cd01a733
2729400n <2729400n@student.gla.ac.uk>
2025-02-26 14:43:51 +0000

    Updated MWPC Geometry

--- Numerical_Methods/Boundaries/task4/boundary.py
+++ Numerical_Methods/Boundaries/task4/boundary.py
@@ -14,36 +14,30 @@ class GeometryFactory(typing.Callable):
         self.V_negative: 'str' = None
 
 
-def geometryFactory(radius, seperation, plate_seperation, V, cx, cy:'float|str'='centre'):
+def geometryFactory(radius, seperation, plate_seperation, V, cx, cy:'float|str'=0.0, center:bool=True,NNCount:int=1,dx:float=1.0,dy:float=1.0):
     cache:'list' = []
-    def bounds(grid:np.ndarray, x_axis:bool = True, x_offset=0, y_offset=0, NNCount:int=3):
-        nonlocal cache
+    def bounds(Grid:np.ndarray, x_axis:bool = True, x_offset=0, y_offset=0):
+        nonlocal cache,cy,cx,center,radius,seperation,plate_seperation,V,NNCount
+        grid = Grid
         grid_shape = grid.shape
         
-        
-        if isinstance(cy,str):
-            if cy == 'centre':
+        if center:
                 cy = grid.shape[0]/2
+                cx = grid.shape[1]/2
+            
+                
         center_distance = 2*np.abs(radius)+np.abs(seperation)
-        print(center_distance)
         cx_s=cx+np.array([center_distance*i for i in range(-NNCount,1+NNCount,1)])
         circles = []
-        canvas = np.ones_like(grid)
+        canvas = np.zeros_like(grid)
         for cx_i in cx_s:
             c1 = geometry.circle(cx_i, cy, radius, 1.0, 1.0,
                                 val=1.0, fill=True, clear=True,Grid=tuple(grid.shape))
             circles.append(c1)
             canvas = canvas*c1
-        canvas[:,(0,-1)]=-V
         grid[:,:] = canvas[:,:]
+        grid[:2,:] =1
+        grid[:-2,:] =1
         return grid
     return bounds
 
-
-if __name__ == "__main__":
-    EGrid = np.ones((500,500))
-    EGrid=bounds(EGrid,40,40,90,5,100,'centre',True,0,0,3)
-    plt.figure(figsize=(15,15))
-    plt.imshow(EGrid)
-    plt.colorbar()
-    plt.show()
