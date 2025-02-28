a88e38675d02ed97459c23734fff37689d019802
2729400n <2729400n@student.gla.ac.uk>
2025-02-28 11:56:44 +0000

    Added cache files handeling

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
 import Numerical_Methods.GUI as gui
-import test_Numerical.check_user_approval as check_user_approval
+import Numerical_Methods.utils.check_user_approval as check_user_approval
 import Numerical_Methods.utils.realtime as rtLoader
-import sys.
-
+import Numerical_Methods.utils.cleancaches as cachecleaner
+import sys
+import Numerical_Methods
+import pathlib
 class TestGUI:
     """A Class used for testing implementation of the GUI."""
 
@@ -17,9 +19,30 @@ class TestGUI:
 
 
 if __name__ == "__main__":
+    
+    clear_cache  = check_user_approval.wouldYouLike(
+        "Would you like to clear cached files in this Package?"
+    )
+    if clear_cache:
+        numerical_methods_path =pathlib.Path(Numerical_Methods.__file__).resolve().absolute()
+        if(numerical_methods_path.is_dir()):
+            cachecleaner.clearPythonCaches(numerical_methods_path)
+        else:
+            cachecleaner.clearPythonCaches(numerical_methods_path.parent)
+            
+    
+    caching = check_user_approval.wouldYouLike(
+        "Would you like to enable Python caching? It makes for speedy re runs!"
+    )
+    
+    sys.dont_write_bytecode=caching
+    
+    
     enable_rtloader = check_user_approval.wouldYouLike(
         "Would you like to enable RTLoader?\nIt allows for real-time reloading of Python modules!"
     )
+    
+    
 
     if enable_rtloader:
         rtLoader.init_realtime_module()
